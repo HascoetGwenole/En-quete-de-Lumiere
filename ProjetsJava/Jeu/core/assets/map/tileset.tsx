51e98779e7ebb7d890f184da37833c2440dc46da
HascoetGwenole <35890353+HascoetGwenole@users.noreply.github.com>
2019-01-11 18:28:45 +0100

    Add files via upload

---
+++ ProjetsJava/Jeu/core/assets/map/tileset.tsx
@@ -0,0 +1,205 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="256" tileheight="261" tilecount="67" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="64" source="baignoire.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="64" source="buffet.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="carrelage1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="cheminee.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="coinplafond.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="218" source="grandeFenetre1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="grass.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="128" source="meublesalledebain.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="mur1bas.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="mur1haut.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="mur2bas.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="mur2haut.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="mur3bas.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="mur3haut..png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="murPierre1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="murPierre2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="parquet1.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="parquet2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="parquet3.png"/>
+ </tile>
+ <tile id="24">
+  <image width="128" height="64" source="pianoAntoine1.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="plafond.png"/>
+ </tile>
+ <tile id="27">
+  <image width="128" height="128" source="riteVaudou.png"/>
+ </tile>
+ <tile id="28">
+  <image width="256" height="128" source="tableReception.png"/>
+ </tile>
+ <tile id="29">
+  <image width="16" height="16" source="tourporte1.png"/>
+ </tile>
+ <tile id="30">
+  <image width="128" height="218" source="murGrand.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="brickExterieur.png"/>
+ </tile>
+ <tile id="32">
+  <image width="128" height="128" source="brickExterieurFenetre.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="plafondPorte.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="tableau1.png"/>
+ </tile>
+ <tile id="37">
+  <image width="64" height="64" source="meubleCuisine1.png"/>
+ </tile>
+ <tile id="38">
+  <image width="64" height="64" source="meubleCuisine2.png"/>
+ </tile>
+ <tile id="39">
+  <image width="128" height="64" source="setCasseroles.png"/>
+ </tile>
+ <tile id="41">
+  <image width="16" height="16" source="aliment.png"/>
+ </tile>
+ <tile id="42">
+  <image width="16" height="16" source="walk.png"/>
+ </tile>
+ <tile id="43">
+  <image width="128" height="64" source="tableCuisine.png"/>
+ </tile>
+ <tile id="44">
+  <image width="64" height="128" source="sapin1.png"/>
+ </tile>
+ <tile id="45">
+  <image width="32" height="32" source="gravier.png"/>
+ </tile>
+ <tile id="46">
+  <image width="32" height="32" source="gravierCote.png"/>
+ </tile>
+ <tile id="47">
+  <image width="96" height="85" source="fontaine.png"/>
+ </tile>
+ <tile id="48">
+  <image width="32" height="64" source="fleur1.png"/>
+ </tile>
+ <tile id="49">
+  <image width="64" height="64" source="banc.png"/>
+ </tile>
+ <tile id="53">
+  <image width="31" height="75" source="characters/jake.png"/>
+ </tile>
+ <tile id="55">
+  <image width="27" height="81" source="characters/Anna.png"/>
+ </tile>
+ <tile id="56">
+  <image width="25" height="78" source="characters/perso1 Clarisse.png"/>
+ </tile>
+ <tile id="57">
+  <image width="30" height="86" source="characters/Jamy.png"/>
+ </tile>
+ <tile id="60">
+  <image width="35" height="120" source="horloge.png"/>
+ </tile>
+ <tile id="61">
+  <image width="32" height="64" source="pileLivre1.png"/>
+ </tile>
+ <tile id="62">
+  <image width="32" height="64" source="pileLivre2.png"/>
+ </tile>
+ <tile id="63">
+  <image width="32" height="64" source="pileLivre3.png"/>
+ </tile>
+ <tile id="64">
+  <image width="32" height="64" source="pileLivre4.png"/>
+ </tile>
+ <tile id="65">
+  <image width="128" height="128" source="tableauTroueAncien.png"/>
+ </tile>
+ <tile id="66">
+  <image width="130" height="160" source="bibli.png"/>
+ </tile>
+ <tile id="71">
+  <image width="106" height="171" source="Lit.png"/>
+ </tile>
+ <tile id="72">
+  <image width="64" height="128" source="billard.png"/>
+ </tile>
+ <tile id="73">
+  <image width="220" height="261" source="tapis.png"/>
+ </tile>
+ <tile id="77">
+  <image width="170" height="108" source="Bureau.png"/>
+ </tile>
+ <tile id="78">
+  <image width="70" height="70" source="Chaise bureau.png"/>
+ </tile>
+ <tile id="79">
+  <image width="140" height="192" source="Piano.png"/>
+ </tile>
+ <tile id="81">
+  <image width="60" height="44" source="Siege Piano.png"/>
+ </tile>
+ <tile id="83">
+  <image width="200" height="128" source="tapisPiskel.png"/>
+ </tile>
+ <tile id="86">
+  <image width="150" height="99" source="Tableau chambre.png"/>
+ </tile>
+ <tile id="88">
+  <image width="16" height="22" source="Table de nuit.png"/>
+ </tile>
+ <tile id="89">
+  <image width="100" height="75" source="Commode.png"/>
+ </tile>
+ <tile id="91">
+  <image width="40" height="75" source="miroir.png"/>
+ </tile>
+ <tile id="93">
+  <image width="140" height="112" source="tableau suite.png"/>
+ </tile>
+ <tile id="94">
+  <image width="40" height="73" source="Trophee.png"/>
+ </tile>
+ <tile id="95">
+  <image width="216" height="150" source="Tapis suite.png"/>
+ </tile>
+</tileset>
