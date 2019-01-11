2f4cf9854ba07a533f1bd378348ebf107ee2adf2
HascoetGwenole <35890353+HascoetGwenole@users.noreply.github.com>
2019-01-11 18:32:24 +0100

    Add files via upload

---
+++ ProjetsJava/Jeu/core/assets/map64x64/mapTileSet.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mapTileSet" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="parquet1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="walk.png"/>
+ </tile>
+</tileset>
